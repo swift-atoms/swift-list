e48234bcef6704e7dc483be85f79b88cc9e87ffd
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-08 00:13:00 +0200

    Use descriptive sentences for test and suite names

--- Tests/List Tests/List Tests.swift
+++ Tests/List Tests/List Tests.swift
@@ -2,14 +2,14 @@ import Testing
 
 @testable import List
 
-@Suite("List")
-struct Tests {
-    @Suite struct Unit {}
-    @Suite struct `Edge Case` {}
-    @Suite struct Integration {}
+@Suite
+struct `List exposes its namespace` {
+    @Suite struct `The List namespace can be referenced` {}
+    @Suite struct `No list namespace boundary cases are defined` {}
+    @Suite struct `No list namespace integration cases are defined` {}
 }
 
-extension Tests.Unit {
+extension `List exposes its namespace`.`The List namespace can be referenced` {
     @Test func `namespace is available`() {
 
         _ = List<Int>.self
